1382e351a4e691c76030269aa0bb3377879e8242
Shameem1212 <shameempourostadkar@gmail.com>
2025-02-02 09:25:32 -0800

    Weather Routes Done

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -1,19 +1,41 @@
 import { Router, type Request, type Response } from 'express';
 const router = Router();
 
-// import HistoryService from '../../service/historyService.js';
-// import WeatherService from '../../service/weatherService.js';
+import HistoryService from '../../service/historyService.js';
+import WeatherService from '../../service/weatherService.js';
 
 // TODO: POST Request with city name to retrieve weather data
 router.post('/', (req: Request, res: Response) => {
   // TODO: GET weather data from city name
+  console.log(req.body, "POSt")
+  try {
+    const city = req.body.cityName;
+    WeatherService.getWeatherForCity(city).then(data => {
+      HistoryService.addCity(city).then(data: any => {
+        console.log(data)
+      })
+    })
+  } catch (err) {
+    console.log("Errr", err)
+  }
   // TODO: save city to search history
 });
 
 // TODO: GET search history
-router.get('/history', async (req: Request, res: Response) => {});
+router.get('/history', async (req: Request, res: Response) => {
+  HistoryService.getCities()
+    .then(data => {
+      return res.json(data)
+    })
+  
+ });
 
 // * BONUS TODO: DELETE city from search history
-router.delete('/history/:id', async (req: Request, res: Response) => {});
+router.delete('/history/:id', async (req: Request, res: Response) => { 
+  HistoryService.removeCity(req.params.id)
+  .then(resp => {
+    res.json({"message":"City deleted"})
+  })
+});
 
 export default router;
--- server/src/service/weatherService.ts
+++ server/src/service/weatherService.ts
@@ -25,7 +25,7 @@ class WeatherService {
   // TODO: Complete buildForecastArray method
   // private buildForecastArray(currentWeather: Weather, weatherData: any[]) {}
   // TODO: Complete getWeatherForCity method
-  // async getWeatherForCity(city: string) {}
+   async getWeatherForCity(city: string) {}
 }
 
 export default new WeatherService();
